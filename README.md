6927d982e472f6a6d41227db900720571cb04474
Eduardo Campaña <hi@orballo.dev>
2024-07-23 00:01:13 +0200

    [wip]

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# runtime
+# AWS Lambda Runtime for WP
 
-AWS Lambda Runtime for WP
+## Introduction
+
+Experimental runtime to run WP on AWS Lambda.
+
+## Status
+
+This is a proof of concept in early development. For the implementation there have been several attempts. One of such was using a customized Bref runtime (written in PHP). This was the first successful attempt on running WP with basic functionalities working. However, it had a very slow startup time which made it unusable for production in cold starts. 
+
+After that I tried several approaches with a custom runtime written in Rust and built on top of AWS's [`lambda-http`](https://github.com/awslabs/aws-lambda-rust-runtime). The main idea was to stop using `php-fpm`, as in a function we don't need a process manager, and use some other PHP runtime.
+
+I went through different ideas while looking for docs, such as using FastCGI without the process manager, then I thought of using LiteSpeed, but in the end, I didn't like the idea to have to start 2 different processes in the function.
+
+Then I decided to go with the difficult but apparently optimal approach, which is to start the PHP's interpreter in the process of the runtime written in Rust. This is the most recent code, though it didn't go to far before I had to stop. It lives in `php-embed-sys`. I tried to write a safe abstraction in Rust over the C API, but it was too difficult, as I had to implement many unnecessary types and functions. Finally I realized that I need to write a wrapper in C to abstract the Zend Engine API, and then write the safe abstraction over the wrapper.
+
+Note: Recently I learned that the wrapper in C is how WordPress Playground implemented this, so I plan on trying to build some POC using Playground on serverless functions on top of Node.js, and once I see how it performs, I'll probably come back to this project.
--- fastcgi/src/protocol.rs
+++ fastcgi/src/protocol.rs
@@ -1,59 +1,59 @@
-// #[repr(u8)]
-// #[derive(Debug, Clone, Copy)]
-// pub enum Role {
-//     Responder = 1,
-//     Authorizer,
-//     Filter,
-// }
+#[repr(u8)]
+#[derive(Debug, Clone, Copy)]
+pub enum Role {
+    Responder = 1,
+    Authorizer,
+    Filter,
+}
 
-// pub enum RequestType {
-//     BeginRequest = 1,
-//     AbortRequest, // Not supported by PHP
-//     EndRequest,
-//     Params,
-//     Stdin,
-//     Stdout,
-//     Stderr,
-//     Data, // Not supported by PHP
-//     GetValues,
-//     GetValuesResult,
-// }
+pub enum RequestType {
+    BeginRequest = 1,
+    AbortRequest, // Not supported by PHP
+    EndRequest,
+    Params,
+    Stdin,
+    Stdout,
+    Stderr,
+    Data, // Not supported by PHP
+    GetValues,
+    GetValuesResult,
+}
 
-// pub enum ProtocolStatus {
-//     RequestComplete = 0,
-//     CantMultiplexConnection,
-//     Overloaded,
-//     UnknownRole,
-// }
+pub enum ProtocolStatus {
+    RequestComplete = 0,
+    CantMultiplexConnection,
+    Overloaded,
+    UnknownRole,
+}
 
-// pub struct Header {
-//     version: u8,
-//     r#type: u8,
-//     request_id: u16,
-//     content_length: u16,
-//     padding_length: u8,
-//     reserved: u8, // Always initialize to 0
-// }
+pub struct Header {
+    version: u8,
+    r#type: u8,
+    request_id: u16,
+    content_length: u16,
+    padding_length: u8,
+    reserved: u8, // Always initialize to 0
+}
 
-// pub struct BeginRequest {
-//     role: u16,
-//     flags: u8,
-//     reserved: [u8; 5], // Always initialize to 0
-// }
+pub struct BeginRequest {
+    role: u16,
+    flags: u8,
+    reserved: [u8; 5], // Always initialize to 0
+}
 
-// pub struct BeginRequestRecord {
-//     header: Header,
-//     body: BeginRequest,
-// }
+pub struct BeginRequestRecord {
+    header: Header,
+    body: BeginRequest,
+}
 
-// pub struct EndRequest {
-//     app_status: u32,
-//     protocol_status: u8,
-//     reserved: [u8; 3], // Always initialize to 0
-// }
+pub struct EndRequest {
+    app_status: u32,
+    protocol_status: u8,
+    reserved: [u8; 3], // Always initialize to 0
+}
 
-// pub struct EndRequestRecord {
-//     header: Header,
-//     body: EndRequest,
-// }
+pub struct EndRequestRecord {
+    header: Header,
+    body: EndRequest,
+}
 
